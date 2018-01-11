1a1ee2f63b3eb0c4707302d9da2adf343f0636e0
Tony Bowden <tony@tmtm.com>
2018-01-11 16:14:28 +0000

    Display population of FLACS on the country page

--- lib/query/country_divisions.rb
+++ lib/query/country_divisions.rb
@@ -10,23 +10,23 @@ module Query
     end
 
     def data
-      division_results.map { |r| Item.new(r[:item], r[:itemLabel]) }
+      division_results.map { |r| Item.new(r[:item], r[:itemLabel], r[:population]) }
     end
 
     private
 
     attr_reader :id
 
-    Item = Struct.new(:id, :name)
+    Item = Struct.new(:id, :name, :population)
 
     def sparql
       @sparql ||= <<~EOQ
-        SELECT DISTINCT ?item ?itemLabel WHERE
+        SELECT DISTINCT ?item ?itemLabel ?population WHERE
         {
           ?item wdt:P31/wdt:P279* wd:Q10864048 ; wdt:P17 wd:#{id} .
           OPTIONAL { ?item wdt:P1082 ?population }
           FILTER NOT EXISTS { ?item wdt:P576 [] }
-          SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
+          SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
         }
         ORDER BY DESC(?population)
       EOQ
