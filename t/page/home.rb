3b7b64c9f9343580b9594801c66059ae67863772
Tony Bowden <tony@tmtm.com>
2018-02-02 09:04:51 +0000

    Get homepage test running again
    This is still very brittle as it depends on live Wikidata queries, and
    we might want to split out tests for the Query classes separately from
    the Page classes, but the first priority is simply to get the tests to
    pass.

--- t/page/home.rb
+++ t/page/home.rb
@@ -1,24 +1,25 @@
 # frozen_string_literal: true
 
 require 'test_helper'
-require_relative '../../lib/page/home'
+
+require_rel '../../lib'
 
 describe 'Homepage' do
-  subject { Page::Home.new }
+  subject { Page::Home.new(countries: Query::CountryList.new.data) }
 
   describe 'countries' do
-    it 'should include Colombia' do
+    it 'should include free countries' do
       subject.countries.map(&:name).must_include 'Colombia'
     end
 
-    it 'should not include Kosovo' do
-      subject.countries.map(&:name).wont_include 'Kosovo'
+    it 'should not include non-free countries' do
+      subject.countries.map(&:name).wont_include 'China'
     end
   end
 
   describe 'title' do
     it 'should give the country count in the title' do
-      subject.title.must_include '193 countries'
+      subject.title.must_include '145 countries'
     end
   end
 end
