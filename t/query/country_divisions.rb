37714a62853a978e2c85b0b23cb782b549aa9d18
Henare Degan <henare.degan@gmail.com>
2018-02-08 14:58:37 +1100

    Add Query::CountryDivisions tests

---
+++ t/query/country_divisions.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+require_rel '../../lib/query/country_info'
+
+describe Query::CountryDivisions do
+  describe 'Estonia (Q191)' do
+    before { VCR.insert_cassette('CountryDivisions Q191') }
+    after { VCR.eject_cassette }
+
+    let(:estonia_divisions) { Query::CountryDivisions.new(id: 'Q191') }
+
+    describe 'Harju County' do
+      subject { estonia_divisions.data.find { |c| c.id == 'Q180200' } }
+
+      it 'should know its name' do
+        subject.name.must_equal 'Harju County'
+      end
+
+      it 'should know its population' do
+        subject.population.must_equal '575601'
+      end
+
+      it 'should know its head of government' do
+        subject.head.name.must_equal 'Ülle Rajasalu'
+      end
+    end
+  end
+end
