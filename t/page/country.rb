344378073e306b5d7a0e5574f6e08f441a92d307
Henare Degan <henare.degan@gmail.com>
2018-02-09 11:56:36 +1100

    Remove duplicated tests from Page::Country
    These tests are now being done against their respective Query objects so
    they're not needed here any more. We're now only testing the
    functionality specific to this Page class.

--- t/page/country.rb
+++ t/page/country.rb
@@ -3,63 +3,12 @@
 require 'test_helper'
 require_relative '../../lib/page/country'
 
-def country_page(qid)
-  country = Query::CountryInfo.new(id: qid).data
-  divisions = Query::CountryDivisions.new(id: qid).data
-  cities = Query::CountryCities.new(id: qid).data
-  Page::Country.new(country: country, divisions: divisions, cities: cities)
-end
-
 describe 'Estonia Page' do
-  around { |test| VCR.use_cassette('Estonia Page', &test) }
-
-  subject { country_page('Q191') }
-
-  describe 'Country data' do
-    it 'should know its name' do
-      subject.country.name.must_equal 'Estonia'
-    end
-
-    it 'should know its population' do
-      subject.country.population.must_equal '1315635'
-    end
-
-    it 'should know its head of government' do
-      subject.country.head.name.must_equal 'Jüri Ratas'
-    end
-
-    it 'should know its head of government office' do
-      subject.country.office.name.must_equal 'Prime Minister of Estonia'
-    end
-
-    it 'should know its legislature' do
-      subject.country.legislature.name.must_equal 'Riigikogu'
-    end
+  let(:country_estonia) do
+    country_estonia = Minitest::Mock.new
+    country_estonia.expect :name, 'Estonia'
   end
-
-  describe 'Divisions data' do
-    let(:harju) { subject.divisions.find { |c| c.id == 'Q180200' } }
-
-    it 'should know some divisions' do
-      harju.name.must_equal 'Harju County'
-    end
-
-    it 'should know info about cities' do
-      harju.head.name.must_equal 'Ülle Rajasalu'
-    end
-  end
-
-  describe 'City data' do
-    let(:tallinn) { subject.cities.find { |c| c.id == 'Q1770' } }
-
-    it 'should know some cities' do
-      tallinn.name.must_equal 'Tallinn'
-    end
-
-    it 'should know info about cities' do
-      tallinn.office.name.downcase.must_equal 'mayor of tallinn'
-    end
-  end
+  subject { Page::Country.new(country: country_estonia, divisions: Minitest::Mock.new, cities: Minitest::Mock.new) }
 
   describe 'title' do
     it 'should give the country name in the title' do
