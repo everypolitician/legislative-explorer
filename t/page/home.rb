dd8cecbf578ad6940715fb35a4179e0433a34fb6
Henare Degan <henare.degan@gmail.com>
2018-02-09 11:57:15 +1100

    Remove duplicated Homepage tests
    These tests are now being done against the Query::CountryList object so
    they're not needed here any more. We're now only testing the
    functionality specific to this Page class.

--- t/page/home.rb
+++ t/page/home.rb
@@ -5,21 +5,9 @@ require 'test_helper'
 require_rel '../../lib'
 
 describe 'Homepage' do
-  around { |test| VCR.use_cassette('Homepage', &test) }
-
-  subject { Page::Home.new(countries: Query::CountryList.new.data) }
-
-  describe 'countries' do
-    it 'should include free countries' do
-      subject.countries.map(&:name).must_include 'Colombia'
-    end
-
-    it 'should not include non-free countries' do
-      subject.countries.map(&:name).wont_include 'China'
-    end
-  end
-
   describe 'title' do
+    subject { Page::Home.new(countries: Array.new(145)) }
+
     it 'should give the country count in the title' do
       subject.title.must_include '145 countries'
     end
