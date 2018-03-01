2b5b2e4ab1ecd3a15a392e37614c7f8bd737bbbc
Henare Degan <henare.degan@gmail.com>
2018-03-01 12:06:44 +1100

    Add tests for show_population helper

---
+++ t/helpers/html_helper.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+require_relative '../../lib/html_helper'
+
+describe HTMLHelper do
+  subject do
+    Class.new { include HTMLHelper }
+  end
+
+  describe 'show_population' do
+    it 'should show a single population from a String' do
+      subject.new.show_population('123').must_equal '(pop. 123)'
+    end
+
+    it 'should show a single population from an Array' do
+      subject.new.show_population(['123']).must_equal '(pop. 123)'
+    end
+
+    it 'should show multiple population' do
+      subject.new.show_population(%w[123 456]).must_equal '(pop. 123 <em>or</em> 456)'
+    end
+  end
+end
